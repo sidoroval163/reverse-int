29d84ddcaecb3bff434e635e185e2178c9bd91ae
sidoroval163 <alex.sid.92@mail.ru>
2020-03-08 15:04:09 +0300

    init:start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+   let z=String (n);
+    z = z.split('');
+    z = z.reverse();
+    z = z.join('');
+    
+    if (n<0){return z.slice(0,-1);}
+    else {return(z);};
 }
